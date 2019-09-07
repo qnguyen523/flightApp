4a3f32212c113970c9f14ddd754a3feca8208c8c
Quoc <qnguyen523@gmail.com>
2019-09-06 22:59:53 -0700

    finish seeding flights

--- flightApp/Models/Flight.cs
+++ flightApp/Models/Flight.cs
@@ -6,12 +6,16 @@ namespace flightApp.Models
     public class Flight
     {
         public int ID { get; set; }
+        [Display(Name = "Departure")]
         public string departure { get; set; }
+        [Display(Name = "Destination")]
         public string destination { get; set; }
 
-        [DataType(DataType.Date)]
+        [Display(Name = "Departure Date")]
+        [DataType(DataType.DateTime)]
         public DateTime departureTime { get; set; }
-        [DataType(DataType.Date)]
+        [Display(Name = "Arrival Date")]
+        [DataType(DataType.DateTime)]
         public DateTime arrivalTime { get; set; }
         public string Class { get; set; }
         public decimal Price { get; set; }
---
+++ flightApp/Models/SeedData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace flightApp.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new FlightContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<FlightContext>>()))
+            {
+                // Look for any flights.
+                if (context.flight.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                context.flight.AddRange(
+                    new Flight
+                    {
+                        departure = "LAX",
+                        destination = "SGN",
+                        departureTime = new DateTime(2019, 8, 18, 16, 30, 0),
+                        arrivalTime = new DateTime(2019, 8, 19, 17, 30, 0),
+                        Class = "Economy",
+                        Price = 120M
+                    },
+                    new Flight
+                    {
+                        departure = "SNA",
+                        destination = "SGN",
+                        departureTime = new DateTime(2019, 8, 19, 16, 30, 0),
+                        arrivalTime = new DateTime(2019, 8, 20, 17, 30, 0),
+                        Class = "Economy",
+                        Price = 120M
+                    },
+                    new Flight
+                    {
+                        departure = "SFO",
+                        destination = "SGN",
+                        departureTime = new DateTime(2019, 8, 20, 16, 30, 0),
+                        arrivalTime = new DateTime(2019, 8, 21, 17, 30, 0),
+                        Class = "Economy",
+                        Price = 120M
+                    },
+                    new Flight
+                    {
+                        departure = "SFO",
+                        destination = "SGN",
+                        departureTime = new DateTime(2019, 8, 21, 16, 30, 0),
+                        arrivalTime = new DateTime(2019, 8, 22, 17, 30, 0),
+                        Class = "Economy",
+                        Price = 120M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
